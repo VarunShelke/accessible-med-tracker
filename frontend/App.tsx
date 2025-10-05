e29e8289ea9f6b185dd5381cebea151f9b918bed
Mohammed Misran <mohammed.misran@pitt.edu>
2025-10-04 22:22:03 -0400

    fix notch padding

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import {BarcodeScanScreen} from './screens/BarcodeScanScreen';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container} edges={['top']}>
-      <BarcodeScanScreen />
-    </SafeAreaView>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top']}>
+        <BarcodeScanScreen />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: 10,
   },
 });
